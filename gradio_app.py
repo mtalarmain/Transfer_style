95a454ca85100c7e463dd1c36e8c14bb9f476927
mtalarmain <mtalarmain@cvc.uab.cat>
2024-07-03 23:32:32 +0200

    comment code main file

--- gradio_app.py
+++ gradio_app.py
@@ -14,26 +14,27 @@ def load_prompt_presets(path_img_style):
         prompt_presets[preset_path.stem] = preset
     return prompt_presets
 
-cache_dir = ".hf_cache/" #path to load Stable Diffusion models
-# use from_pipe to avoid consuming additional memory when loading a checkpoint
+# Path to load Stable Diffusion models
+cache_dir = ".hf_cache/" 
+
+# Load Models
 controlnet = ControlNetModel.from_pretrained("diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16, cache_dir=cache_dir)
 vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16, cache_dir=cache_dir)
 pipe_sdxl_controlnet = StableDiffusionXLControlNetPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", controlnet=controlnet, vae=vae, torch_dtype=torch.float16, cache_dir=cache_dir).to("cuda")
-pipe_sdxl_controlnet.enable_model_cpu_offload()
-#pipe_controlnet.enable_xformers_memory_efficient_attention()
+pipe_sdxl_controlnet.enable_model_cpu_offload() # For better memory efficiency
 
-path_img_style = './prompts/'
-prompt_presets = load_prompt_presets(path_img_style)
-preset_list = list(prompt_presets.keys())
+path_img_style = './prompts/' # Path where image styles are located
+prompt_presets = load_prompt_presets(path_img_style) # Load image style prompts
+preset_list = list(prompt_presets.keys()) # List image styles
 
+# From Input to Image Styles Image
 def diffusion_model(img_input, style_group, steps_slider, guidance_scale, controlnet_conditioning_scale):
     prompt_style_positive = prompt_presets[style_group]['positive']
     prompt_style_negative = prompt_presets[style_group]['negative']
-    image = cv2.Canny(img_input, 100, 200)
+    image = cv2.Canny(img_input, 100, 200) # transform input to canny image
     image = image[:, :, None]
     image = np.concatenate([image, image, image], axis=2)
     canny_image = Image.fromarray(image)
-    #controlnet_conditioning_scale = 0.5  # recommended for good generalization
     image = pipe_sdxl_controlnet(prompt=prompt_style_positive, negative_prompt=prompt_style_negative, image=canny_image, controlnet_conditioning_scale=controlnet_conditioning_scale, guidance_scale = guidance_scale, num_inference_steps=steps_slider).images[0]
 
     return image
